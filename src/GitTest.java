0e39a8091474c71fcd7eeaa2002c00437dca26e4
Mr.Sergey <Forex187000@gmail.com>
2023-01-06 19:29:25 +0300

    Commit from Idea

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Save this changes in Git");
+        System.out.println("One more changes");
     }
 }
