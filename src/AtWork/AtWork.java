e712f9bec6870ca7441b92dcf663b28724774967
MrSergeyAtWork <forex187000@gmail.com>
2023-01-10 10:07:09 +0300

    Changes at work!

---
+++ src/AtWork/AtWork.java
@@ -0,0 +1,8 @@
+package AtWork;
+
+public class AtWork {
+    public static void main(String[] args) {
+        System.out.println("This new text i wrote at work");
+        System.out.println("Also i deleted two java file, which i created from home");
+    }
+}
--- src/GitTest.java
+++
@@ -1,6 +0,0 @@
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("Save this changes in Git");
-        System.out.println("One more changes");
-    }
-}
