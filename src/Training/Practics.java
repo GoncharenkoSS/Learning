0c1c1d8d7c64eb369c33e211a022bd637aaa9aa4
MrSergeyAtWork <forex187000@gmail.com>
2023-01-17 11:39:52 +0300

    Delete Kkal!

--- src/Training/Practics.java
+++ src/Training/Practics.java
@@ -2,18 +2,6 @@ package Training;
 
 public class Practics {
     public static void main(String[] args) {
-    Player one = new Player(91);
-    Player two = new Player(92);
-    Player four = new Player(94);
-    Player.info();
-    Player six = new Player(96);
-    Player seven = new Player(97);
-    Player nine = new Player(99);
-    Player.info();
 
-        System.out.println(nine.getStamina());
-    for(int i=0;i< nine.getStamina();i++) nine.run();
-        Player.info();
-        System.out.println(nine.getStamina());
     }
 }
